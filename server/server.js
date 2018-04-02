08d80c3171749e6a523533012334e356857cd611
or moyal <ormoyall@gmail.com>
2018-04-02 16:03:36 +0300

    add socket io and start use event handlers like connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname + '/../public');
 
 const express = require('express');
+const socketIO = require('socket.io');
 const app = express();
 
 process.env.PORT = process.env.PORT || 3000;
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('user connect to server');
+
+    socket.on('disconnect', () => {
+        console.log('disconnect from browser');
+    })
+
+})
 
 
-app.listen(process.env.PORT, () => {
-    console.log('run on port',process.env.PORT);
+
+server.listen(process.env.PORT, () => {
+    console.log(`run on port ${process.env.PORT}`);
 })
 
 
